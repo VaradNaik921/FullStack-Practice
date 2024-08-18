3163e84f30476bc3f40af97a5b49c5213a2e2815
VaradNaik921 <129571419+VaradNaik921@users.noreply.github.com>
2024-08-18 11:30:00 +0530

    Adding Assignment15:NodeJS(Module Export-Import)

---
+++ assignment/assignment15-nodejs/main.js
@@ -0,0 +1,8 @@
+//Importing functions from operations.js
+const { add, sub, mul, div } = require('./operations.js');
+
+//Using the functions to print results
+console.log("1+2 = ", add(1, 2));
+console.log("4-3 = ", sub(3, 4));
+console.log("5*6 = ", mul(5, 6));
+console.log("7/8 = ", div(7, 8));
---
+++ assignment/assignment15-nodejs/operations.js
@@ -0,0 +1,22 @@
+//Function to add
+let add = (a, b) => {
+    return a + b;
+};
+
+//Function to subtract
+let sub = (a, b) => {
+    return a - b;
+};
+
+//Function to multiply
+let mul = (a, b) => {
+    return a * b;
+};
+
+//Function to divide
+let div = (a, b) => {
+    return a / b;
+};
+
+//Exporting functions to use in main
+module.exports = { add, sub, mul, div };
